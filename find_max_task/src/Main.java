663ccf34733aca77f4bba77904ca95b71fd2f371
hikagarri lovesy <hikagarrilovesy@MacBook-Air-hikagarri.local>
2025-12-28 15:30:52 +0300

    добавил ввод пользователем 5

--- find_max_task/src/Main.java
+++ find_max_task/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     
@@ -18,13 +19,32 @@ public class Main {
     }
     
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введите количество элементов в массиве: ");
+        int count = scanner.nextInt();
+        
+        if (count <= 0) {
+            System.out.println("Количество элементов должно быть положительным!");
+            return;
+        }
+        
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(10);
-        numbers.add(5);
-        numbers.add(20);
-        numbers.add(15);
+        
+        System.out.println("Введите " + count + " элементов:");
+        for (int i = 0; i < count; i++) {
+            System.out.print("Элемент " + (i + 1) + ": ");
+            numbers.add(scanner.nextInt());
+        }
         
         Integer max = findMax(numbers);
-        System.out.println("Максимальное значение: " + max);
+        
+        if (max != null) {
+            System.out.println("Максимальное значение в массиве: " + max);
+        } else {
+            System.out.println("Не удалось найти максимальное значение");
+        }
+        
+        scanner.close();
     }
 }
